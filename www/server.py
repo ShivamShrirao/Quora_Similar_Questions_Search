6fffe01be2c9f9d025c5825aabca8735bb2f7f75
ShivamShrirao <shivamshrirao@gmail.com>
2021-01-19 18:00:39 +0530

    basic search functionality working

--- www/server.py
+++ www/server.py
@@ -6,11 +6,12 @@ import torch
 import numpy as np
 import json
 import pickle
+import time
 
 DATASET_DIR = "../quora-question-pairs-dataset/"
 
 with open (DATASET_DIR + 'corpus_sentences.list', 'rb') as fp:
-    corpus_sentences = pickle.load(fp)
+	corpus_sentences = pickle.load(fp)
 
 corpus_embeddings = torch.load(DATASET_DIR+'corpus_embeddings.pt')
 
@@ -24,27 +25,30 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+def search_question(inp_question, top_k=10):
+	question_embedding = model.encode(inp_question, convert_to_tensor=True)
+	hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=top_k)
+	return hits[0]
+
+cache_hits = {}
+# TODO: Limit cache_hits size.
 def get_resp_dicts(query):
 	res = []
-	hits = search_question(query)
+	hits = cache_hits.get(query)
+	if hits is None:
+		hits = search_question(query)
+		cache_hits[query] = hits
 	for hit in hits:
 		res.append({
 				"value": corpus_sentences[hit['corpus_id']],
 				"score": f"({hit['score']:.3f}) ",
-				"url": url_for('about', id=hit['corpus_id'])
+				"url": url_for('about', qid=hit['corpus_id'])
 				})
 	return res
 
-@app.route('/fetch')
-def submit():
-	query = request.args['term']
-	res = []
-	if query:
-		res = get_resp_dicts(query)
-	return json.dumps(res)
-
 @app.route('/search')
 def search():
+	start_time = time.time()
 	res = []
 	query = None
 	try:
@@ -53,37 +57,14 @@ def search():
 			res = get_resp_dicts(query)
 	except KeyError:
 		pass
-	return render_template("search.html", qtype="Search", res=res, query=query)
+	end_time = time.time()
+	time_taken = end_time-start_time
+	return render_template("search.html", qtype="Search", res=res, query=query, time_taken=f"{time_taken:.4f}")
 
 
-@app.route('/about/<id>')
-def about(id):
-	ret = db.movies.find_one({"id": int(id)}, {"_id": 0})
-	similar = []
-	if ret is None:
-		ret = {"title": "Not Found!"}
-	else:
-		top_results, cos_scores = get_recommendations(ret["idx"])
-		for i in top_results:
-			similar.append(db.movies.find_one({"idx": i.item()}, {"_id": 0}))
-			similar[-1]["cos_score"] = cos_scores[i].item()
-		similar[:15] = sorted(similar[:15], key=lambda e: e["vote_weight"]*e["cos_score"], reverse=True)
-	return render_template("about.html", main_movie=ret, similar=similar)
-
-
-def search_question(inp_question, top_k=10):
-    start_time = time.time()
-    question_embedding = model.encode(inp_question, convert_to_tensor=True)
-    hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=top_k)
-    end_time = time.time()
-    hits = hits[0]
-
-    print("Input question:", inp_question)
-    print("Results (after {:.3f} seconds):".format(end_time-start_time))
-    for hit in hits:
-        print("\t{:.3f}\t{}".format(hit['score'], corpus_sentences[hit['corpus_id']]))
-
-    return hits
+@app.route('/about/<qid>')
+def about(qid):
+	return render_template("about.html", main_que=corpus_sentences[qid], similar=similar)
 
 
 if __name__ == '__main__':
